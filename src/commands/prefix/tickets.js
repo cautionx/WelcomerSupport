613f162c9629c91011cf8b6b2e5ea1e347fba6e8
caution <jhannan644@gmail.com>
2025-03-12 22:58:31 +0000

    push

--- src/commands/prefix/tickets.js
+++ src/commands/prefix/tickets.js
@@ -3,21 +3,19 @@ const Ticket = require("../../structure/ticket_structure");
 
 module.exports = {
     name: "tickets",
-    description: "Lists all open tickets.",
+    description: "Lists all open tickets in the server.",
     async execute(message, args) {
-        let query = { userId: message.author.id, solved: false };
-
-        const tickets = await Ticket.find(query);
+        const tickets = await Ticket.find({ solved: false });
 
         if (tickets.length === 0) {
-            return message.channel.send("No open tickets found.");
+            return message.channel.send("There are no open tickets.");
         }
 
         const embed = new EmbedBuilder()
-            .setColor('Aqua')
-            .setTitle("Tickets")
+            .setColor('Orange')
+            .setTitle(`Tickets (${tickets.length})`)
             .setDescription(tickets.map(ticket =>
-                `**#${ticket.ticketId}** - <#${ticket.channelId}> (Created: <t:${Math.floor(ticket.createdAt / 1000)}:R>)`
+                `**#${ticket.ticketId}** - <#${ticket.channelId}> - <@${ticket.userId}> (**<t:${Math.floor(ticket.createdAt / 1000)}:R>**)`
             ).join("\n"))
 
         message.channel.send({ embeds: [embed] });
