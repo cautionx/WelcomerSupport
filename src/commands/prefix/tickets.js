308cad8fe5d938b252e1cc523490f6e1f44a30e5
caution <jhannan644@gmail.com>
2025-03-12 23:09:32 +0000

    push

--- src/commands/prefix/tickets.js
+++ src/commands/prefix/tickets.js
@@ -1,11 +1,18 @@
-const { EmbedBuilder } = require("discord.js");
+const { EmbedBuilder, PermissionFlagsBits } = require("discord.js");
 const Ticket = require("../../structure/ticket_structure");
-const config = require('../../../config');
+const config = require("../../../config");
+const permissions = require("../../config/perm.json");
 
 module.exports = {
     name: "tickets",
     description: "Lists all open tickets in the server.",
     async execute(message, args) {
+        const helperRoleId = permissions.helperRole;
+
+        if (!message.member.roles.cache.has(helperRoleId)) {
+            return;
+        }
+
         const tickets = await Ticket.find({ solved: false });
 
         if (tickets.length === 0) {
@@ -17,7 +24,7 @@ module.exports = {
             .setTitle(`Tickets (${tickets.length})`)
             .setDescription(tickets.map(ticket =>
                 `**#${ticket.ticketId}** [<#${ticket.channelId}> -- <@${ticket.userId}>] (**<t:${Math.floor(ticket.createdAt / 1000)}:R>**)`
-            ).join("\n"))
+            ).join("\n"));
 
         message.channel.send({ embeds: [embed] });
     }
